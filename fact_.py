141535dea2c9eb2fcb334c58a83cf912f53d8a6e
oxsh0gunX <146795260+oxsh0gunX@users.noreply.github.com>
2025-11-19 11:39:30 +0530

    day2

---
+++ fact_.py
@@ -0,0 +1,18 @@
+# 5!=5*4*3*2*1
+#usr input
+#result=1
+#for i=num;i>=0;i--
+# result*=i
+# range=int(input("Enter the number "))
+# result=1
+# int(result)=result
+# for num in range(range,0,-1):
+#     result*=num
+# print(result)
+range_=int(input("enter the number here :"))
+result=1
+for number in range(1,range_+1):
+    # print(number)
+    result*=number
+print(result)
+    
