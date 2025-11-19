09b5a9586168ae155feab3ec408ed501501be52e
oxsh0gunX <146795260+oxsh0gunX@users.noreply.github.com>
2025-11-19 11:00:32 +0530

    day2

---
+++ Swap_2nd_2string.py
@@ -0,0 +1,12 @@
+#cat  cot
+# dog dag
+string1=input("Enter 2 string ")
+string2=input()
+
+str1_second_char=string1[1]
+str2_second_char=string2[1]
+print(string1[0] + string2[1] + string1[2:])
+print(string2[0] + string1[1] + string2[2:])
+
+
+
