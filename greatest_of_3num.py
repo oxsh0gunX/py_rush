a08278818c12e0ee8feefdeaab42243b6370ec33
oxsh0gunX <146795260+oxsh0gunX@users.noreply.github.com>
2025-11-19 10:14:30 +0530

    Create greatest_of_3num.py

---
+++ greatest_of_3num.py
@@ -0,0 +1,10 @@
+#bg of 3num
+a=int(input("input 3 numbers"))
+b=int(input())
+c=int(input())
+if a>b and a>c:
+    print(f"{a} is greater ")
+elif b>a and b>c:
+    print(f"{b} is greater")
+else:
+    print(f"{c} is greter")
