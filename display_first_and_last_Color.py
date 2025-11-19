7b6ee1baef647649d66ac47302d4cc2ca62b73e4
oxsh0gunX <146795260+oxsh0gunX@users.noreply.github.com>
2025-11-19 10:22:59 +0530

    day2

---
+++ display_first_and_last_Color.py
@@ -0,0 +1,5 @@
+color=input("Enter the color name suprate by the comma ")
+split_color=color.split(',')
+fisrt_color=split_color[0]
+last_color=split_color[-1]
+print(f"The first color is {fisrt_color} and the last color is {last_color}")
