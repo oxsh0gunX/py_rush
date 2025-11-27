b474a732c8b17af1213d4d50ca87849d1c8ef7fe
oxsh0gunX <146795260+oxsh0gunX@users.noreply.github.com>
2025-11-27 12:01:35 +0530

    test

---
+++ class_declare.py
@@ -0,0 +1,10 @@
+class test:
+    def __init__(self):
+        self.start=15
+    def loop(self):
+        for x in range(self.start):
+            print(x)
+            
+my_class=test()
+my_class.loop()
+    # def loop(self)
